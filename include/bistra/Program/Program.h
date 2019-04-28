51d646c9eceb73c713391738285f5d96dccf5804
Nadav Rotem <nadav256@gmail.com>
2019-04-27 22:05:20 -0700

    Convert the type to use std::vector to store the dims.

--- include/bistra/Program/Program.h
+++ include/bistra/Program/Program.h
@@ -5,11 +5,11 @@
 #include <cstdint>
 #include <type_traits>
 #include <utility>
+#include <vector>
 
 namespace bistra {
 
 struct Type;
-constexpr unsigned max_tensor_dimensions = 6;
 
 /// An enum representing the type used by the elements of a tensor.
 enum class ElemKind : unsigned char {
@@ -21,23 +21,14 @@ enum class ElemKind : unsigned char {
 /// A class that represents a type of a tensor.
 struct Type final {
   /// Contains the dimensions (sizes) of the tensor. Ex: [sx, sy, sz, ...].
-  size_t sizes_[max_tensor_dimensions] = {
-      0,
-  };
-
-  /// Contains the number of dimensions used by the tensor.
-  unsigned char numSizes_{0};
+  std::vector<unsigned> sizes_{};
 
   /// Specifies the element type of the tensor.
   ElemKind elementType_{ElemKind::Float32Ty};
 
   /// Initialize a new non-quantized type.
-  Type(ElemKind elemTy, unsigned *dims, unsigned numDims)
-      : numSizes_(numDims), elementType_(elemTy) {
-    for (unsigned i = 0; i < numSizes_; i++) {
-      sizes_[i] = dims[0];
-    }
-  }
+  Type(ElemKind elemTy, const std::vector<unsigned> &dims)
+      : sizes_(dims), elementType_(elemTy) {}
 
   /// An empty type.
   Type() = default;
@@ -49,25 +40,23 @@ struct Type final {
       return false;
     }
     // Must have the same number of sizes.
-    if (numSizes_ != other.numSizes_) {
+    if (sizes_ != other.sizes_) {
       return false;
-    }
-    // Sizes must be the same.
-    for (size_t i = 0; i < numSizes_; i++) {
-      if (sizes_[i] != other.sizes_[i]) {
-        return false;
-      }
     }
 
     return true;
   }
 
+  /// \returns the dimensions of the tensor.
+  const std::vector<unsigned> &dims() { return sizes_; }
+
+  /// \returns the tensor element type.
   ElemKind getElementType() const { return elementType_; }
 
   /// \returns the number of elements in the tensor.
   size_t size() const {
     size_t s = 1;
-    for (unsigned char i = 0; i < numSizes_; i++) {
+    for (unsigned char i = 0, e = sizes_.size(); i < e; i++) {
       s *= size_t(sizes_[i]);
     }
 
