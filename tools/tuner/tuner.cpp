d226cc3cf41c0347954227e56512ae548acb820d
Nadav Rotem <nrotem@fb.com>
2019-04-30 09:31:28 -0700

    Implement function cloning.

--- tools/tuner/tuner.cpp
+++ tools/tuner/tuner.cpp
@@ -4,16 +4,16 @@
 using namespace bistra;
 
 int main() {
-  Program p;
-  p.addArgument("bar", {32, 32}, {"X", "Y"}, ElemKind::Float32Ty);
-  p.addArgument("foo", {10, 32, 32, 4}, {"N", "H", "W", "C"},
-                ElemKind::Float32Ty);
+  Program *p = new Program();
+  p->addArgument("bar", {32, 32}, {"X", "Y"}, ElemKind::Float32Ty);
+  p->addArgument("foo", {10, 32, 32, 4}, {"N", "H", "W", "C"},
+                 ElemKind::Float32Ty);
   auto *L = new Loop("i", 10, 1);
   auto *K = new Loop("j", 10, 1);
   L->addStmt(K);
-  p.addStmt(L);
+  p->addStmt(L);
 
-  auto *buff = &p.getArgs()[0];
+  auto *buff = p->getArgs()[0];
   auto *ld = new LoadExpr(buff, {new IndexExpr(K), new IndexExpr(L)});
   auto *val = new AddExpr(ld, new ConstantExpr(4));
   auto *store =
@@ -21,6 +21,11 @@ int main() {
 
   K->addStmt(store);
 
-  p.dump();
+  Program *pp = p->clone();
+  delete p;
+  for (int i = 0; i < 10; i++) {
+    pp = pp->clone();
+    pp->dump();
+  }
   std::cout << std::endl;
 }
