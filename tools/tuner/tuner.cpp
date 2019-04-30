2239a1e3c20d3181416154bbf09b74f0bde648e6
Nadav Rotem <nrotem@fb.com>
2019-04-29 21:22:10 -0700

    Add scopes to both loop and program and simplified the API.

--- tools/tuner/tuner.cpp
+++ tools/tuner/tuner.cpp
@@ -8,10 +8,10 @@ int main() {
   p.addArgument("foo", {10, 32, 32, 4}, ElemKind::Float32Ty);
   p.addArgument("bar", {32, 32}, ElemKind::Float32Ty);
   auto *L = new Loop("i", 10, 1);
-  auto *S = new Scope({L});
-  p.setBody(S);
   auto *K = new Loop("j", 100, 1);
-  L->setBody(K);
+  L->addStmt(K);
+
+  p.addStmt(L);
   p.dump();
   std::cout << std::endl;
 }
