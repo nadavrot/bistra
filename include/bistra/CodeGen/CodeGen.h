23d7d76e8ac5b3bd834b4a719b4d8390043afcd6
Nadav Rotem <nrotem@fb.com>
2019-05-01 16:55:23 -0700

    Add ExprType to all expressions.

--- include/bistra/CodeGen/CodeGen.h
+++ include/bistra/CodeGen/CodeGen.h
@@ -1,11 +1,6 @@
 #ifndef BISTRA_CODEGEN_CODEGEN_H
 #define BISTRA_CODEGEN_CODEGEN_H
 
-namespace bistra {
-
-/// \returns a CPP program that represents the program \p P.
-std::string emitCPP(Program &P);
-
-} // namespace bistra
+namespace bistra {} // namespace bistra
 
 #endif // BISTRA_CODEGEN_CODEGEN_H
