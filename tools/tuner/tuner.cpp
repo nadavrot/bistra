64cfece725bd1c4c79942d85915f30f7814a8710
Nadav Rotem <nadav256@gmail.com>
2019-05-01 18:30:16 -0700

    Fix the matmul example order of loops.

--- tools/tuner/tuner.cpp
+++ tools/tuner/tuner.cpp
@@ -13,8 +13,8 @@ int main() {
   p->addArgument("B", {64, 32}, {"K", "J"}, ElemKind::Float32Ty);
 
   auto *C = p->getArg(0);
-  auto *B = p->getArg(1);
-  auto *A = p->getArg(2);
+  auto *A = p->getArg(1);
+  auto *B = p->getArg(2);
 
   auto *I = new Loop("i", 128, 1);
   auto *J = new Loop("j", 32, 1);
@@ -24,8 +24,8 @@ int main() {
   I->addStmt(J);
   J->addStmt(K);
 
-  auto *ldB = new LoadExpr(B, {new IndexExpr(I), new IndexExpr(K)});
-  auto *ldA = new LoadExpr(A, {new IndexExpr(K), new IndexExpr(J)});
+  auto *ldA = new LoadExpr(A, {new IndexExpr(I), new IndexExpr(K)});
+  auto *ldB = new LoadExpr(B, {new IndexExpr(K), new IndexExpr(J)});
   auto *mul = new MulExpr(ldA, ldB);
   auto *st = new StoreStmt(C, {new IndexExpr(I), new IndexExpr(J)}, mul, true);
   K->addStmt(st);
