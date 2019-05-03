98a0b1d439b7fb9e6b206feb721c624eb7d584a9
Nadav Rotem <nrotem@fb.com>
2019-05-03 12:21:35 -0700

    Add a program node visitor.

--- tests/unittests/basic.cpp
+++ tests/unittests/basic.cpp
@@ -83,6 +83,48 @@ TEST(basic, memcpy) {
   delete pp;
 }
 
+/// A visitor class that visits all nodes in the program.
+struct NodeCounter : public NodeVisitor {
+  unsigned stmt{0};
+  unsigned expr{0};
+  virtual void handle(Stmt *) { stmt++; }
+  virtual void handle(Expr *) { expr++; }
+};
+
+TEST(basic, visitor_collect_indices) {
+  // C[i, j] = A[i, k] * B[k, j];
+  Program *p = new Program();
+  p->addArgument("C", {128, 256}, {"I", "J"}, ElemKind::Float32Ty);
+  p->addArgument("A", {128, 512}, {"I", "K"}, ElemKind::Float32Ty);
+  p->addArgument("B", {512, 256}, {"K", "J"}, ElemKind::Float32Ty);
+
+  auto *C = p->getArg(0);
+  auto *B = p->getArg(1);
+  auto *A = p->getArg(2);
+
+  auto *I = new Loop("i", 128, 1);
+  auto *J = new Loop("j", 32, 1);
+  auto *K = new Loop("k", 64, 1);
+
+  p->addStmt(I);
+  I->addStmt(J);
+  J->addStmt(K);
+
+  auto *ldB = new LoadExpr(B, {new IndexExpr(I), new IndexExpr(K)});
+  auto *ldA = new LoadExpr(A, {new IndexExpr(K), new IndexExpr(J)});
+  auto *mul = new MulExpr(ldA, ldB);
+  auto *st = new StoreStmt(C, {new IndexExpr(I), new IndexExpr(J)}, mul, true);
+  K->addStmt(st);
+  p->verify();
+
+  NodeCounter counter;
+  p->visit(&counter);
+
+  EXPECT_EQ(counter.stmt, 8);
+  EXPECT_EQ(counter.expr, 9);
+  delete p;
+}
+
 TEST(basic, time_simple_loop) {
   // C[i, j] = A[i, k] * B[k, j];
   Program *p = new Program();
