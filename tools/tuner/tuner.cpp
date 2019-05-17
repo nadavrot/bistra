56522e0c0d7ae2c25623a0a039281fa312199418
Nadav Rotem <nadav256@gmail.com>
2019-05-17 09:16:26 -0700

    Update the tuner example to use 1024**3 matmul example.

--- tools/tuner/tuner.cpp
+++ tools/tuner/tuner.cpp
@@ -12,19 +12,41 @@
 using namespace bistra;
 
 const char *gemmSource = R"(
-def gemm (C:float<I:512,J:512>, A:float<I:512,K:512>, B:float<K:512,J:512>) {
-  for (i in 0 .. 512) {
-    for (j in 0 .. 512) {
-      C[i,j] =  0.000000 ;
-      for (k in 0 .. 512) {
+def gemm (C:float<I:1024,J:1024>, A:float<I:1024,K:1024>, B:float<K:1024,J:1024>) {
+  for (i in 0 .. C.I) {
+    for (j in 0 .. C.J) {
+      //C[i,j] =  0.000000 ;
+      for (k in 0 .. A.K) {
         C[i,j] += A[i,k] * B[k,j];
       }
     }
   }
 }
-})";
+)";
 
 int main() {
   Program *p = parseProgram(gemmSource);
-  optimizeEvaluate(p);
+  auto *I = ::getLoopByName(p, "i");
+  auto *J = ::getLoopByName(p, "j");
+  auto *K = ::getLoopByName(p, "k");
+
+  ::vectorize(J, 8);
+  ::widen(J, 4);
+  ::widen(I, 3);
+
+  ::tile(K, 32);
+  ::hoist(K, 3);
+
+  ::tile(I, 63);
+  ::hoist(I, 3);
+
+  ::simplify(p);
+  ::promoteLICM(p);
+  p->dump();
+
+  auto CB = getBackend("C");
+  writeFile("/tmp/1.cc", CB->emitBenchmarkCode(p, 1));
+
+  auto res = CB->evaluateCode(p, 10);
+  std::cout << "result = " << res << "\n";
 }
