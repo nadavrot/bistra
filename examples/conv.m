cd638e91e963d9a147a3878f8f56e15fdca1522d
Nadav Rotem <nrotem@fb.com>
2019-05-20 15:30:23 -0700

    Update the example code to use constants instead of indices.

--- examples/conv.m
+++ examples/conv.m
@@ -1,12 +1,18 @@
-let kernel = 7
-let stride = 2
-let pad = 3
+let kernel = 3
+let stride = 1
+let pad = 1
+
+let batch = 1
+let channel_in = 128
+let channel_out = 128
+let size_in = 28
+let size_out = 28
 
 def conv(
-  Out:float<N:3, H:112, W:112, C:64>,
-  In:float <N:3, H:224, W:224, C:3>,
-  Filter:float <CI:3, K0:7, K1:7, CO:64>,
-  Bias:float<O:64>) {
+  Out: float<N:batch, H:size_out, W:size_out, C:channel_out>,
+  In: float<N:batch, H:size_in, W:size_in, C:channel_in>,
+  Filter: float<CI:channel_in, K0:kernel, K1:kernel, CO:channel_out>,
+  Bias: float<O:channel_out>) {
 
   for (n in 0 .. Out.N) {
     // For each output channel:
