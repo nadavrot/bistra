99d3a4862c6ca33bfb37c7ef54da5f8f9e7cc702
Nadav Rotem <nrotem@fb.com>
2019-05-21 22:08:04 -0700

    Move more code into Analysis

--- include/bistra/Analysis/Value.h
+++ include/bistra/Analysis/Value.h
@@ -1,11 +1,14 @@
 #ifndef BISTRA_ANALYSIS_VALUE_H
 #define BISTRA_ANALYSIS_VALUE_H
 
+#include <unordered_map>
 #include <utility>
 
 namespace bistra {
 
 class Expr;
+class ASTNode;
+class Stmt;
 
 /// This is similar to LLVM's simplify demanded bits.
 /// Updates the possible range in \p range.
@@ -24,6 +27,13 @@ bool isZero(Expr *e);
 enum RangeRelation { Intersect, Disjoint, Subset };
 /// \returns the relationship between sets A and B.
 RangeRelation getRangeRelation(std::pair<int, int> A, std::pair<int, int> B);
+
+/// Compute cost type: num memory ops, num arithmetic.
+using ComputeCostTy = std::pair<uint64_t, uint64_t>;
+/// Estimate the compute cost for all expressions and statements in \p S by
+/// updating the map \p heatmap.
+void estimateCompute(Stmt *S,
+                     std::unordered_map<ASTNode *, ComputeCostTy> &heatmap);
 
 } // end namespace bistra
 
