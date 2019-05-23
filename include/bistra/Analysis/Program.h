a7cb737be4b93b14bdd0d4c3f1aba4b399dcca31
Nadav Rotem <nadav256@gmail.com>
2019-05-22 21:00:12 -0700

    Add a new library for whole-program analysis.

---
+++ include/bistra/Analysis/Program.h
@@ -0,0 +1,16 @@
+#ifndef BISTRA_ANALYSIS_PROGRAM_H
+#define BISTRA_ANALYSIS_PROGRAM_H
+
+namespace bistra {
+
+class Loop;
+
+/// \returns the estimated the number of elements are loaded in a loop.
+uint64_t getNumLoadsInLoop(Loop *L);
+
+/// \returns the number of arithmetic operations in a loop.
+uint64_t getNumArithmeticInLoop(Loop *L);
+
+} // end namespace bistra
+
+#endif
