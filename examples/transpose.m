374f66e1217eb94427e6638aa07ca31464dda301
Nadav Rotem <nadav256@gmail.com>
2019-05-22 21:01:43 -0700

    Add a transpose matrix example.

---
+++ examples/transpose.m
@@ -0,0 +1,12 @@
+
+let m = 1024
+let n = 1024
+
+def transpose(A:float<m:m, n:n>,
+              B:float<n:n, m:m>) {
+  for (i in 0 .. A.m) {
+    for (j in 0 .. A.n) {
+      A[i,j] = B[j,i];
+    }
+  }
+}
