ab6c0b4db06b2be1fa6ccc2c2f23d95be72897a7
Nadav Rotem <nadav256@gmail.com>
2019-05-26 16:28:19 -0700

    Add batched_add

---
+++ examples/batched_add.m
@@ -0,0 +1,15 @@
+
+let batch = 32
+let sx = 512
+let sy = 512
+
+def batched_add(Out:float<x:sx, y:sy>, In:float<b:batch, x:sx, y:sy>) {
+  for (x in 0 .. In.x) {
+    for (y in 0 .. In.y) {
+      Out[x,y] = 0.0
+        for (b in 0 .. In.b) {
+          Out[x,y] += In[b,x,y]
+        }
+    }
+  }
+}
