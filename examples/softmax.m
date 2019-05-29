9808d3a456f132645922621ec1a2adadb766851b
Nadav Rotem <nrotem@fb.com>
2019-05-29 10:53:33 -0700

    Add a softmax example

---
+++ examples/softmax.m
@@ -0,0 +1,26 @@
+
+def softmax(Out:float<x:100>,
+            In:float<x:100>) {
+    var mx : float
+    mx = In[0]
+
+    // Find Max.
+    for (i in 0 .. 100) {
+      mx = max(mx, In[i]);
+    }
+
+    var sum : float
+
+    // Compute exp.
+    for (i in 0 .. 100) {
+      var e : float
+      e = exp(In[i] - mx)
+      sum += e
+      Out[i] = e
+    }
+
+    // Normalize the output.
+    for (i in 0 .. 100) {
+      Out[i] = Out[i] / sum;
+    }
+}
