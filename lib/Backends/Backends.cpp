6e45d462f05cbec5038f43e2adcea410ac0ec430
Nadav Rotem <nrotem@fb.com>
2019-05-30 09:23:22 -0700

    Implement the llvm code-gen support for simple loops.

--- lib/Backends/Backends.cpp
+++ lib/Backends/Backends.cpp
@@ -1,6 +1,7 @@
 #include "bistra/Backends/Backends.h"
 #include "bistra/Backends/Backend.h"
 #include "bistra/Backends/CBackend/CBackend.h"
+#include "bistra/Backends/LLVMBackend/LLVMBackend.h"
 using namespace bistra;
 
 std::unique_ptr<Backend> bistra::getBackend(const std::string &name) {
