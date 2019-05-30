f9800563321f2a6fdb701e0baf5eb3d70726e82c
Nadav Rotem <nadav256@gmail.com>
2019-05-29 21:27:11 -0700

    Find LLVM and create an empty backend.

---
+++ lib/Backends/LLVMBackend/LLVMBackend.cpp
@@ -0,0 +1,16 @@
+#include "bistra/Backends/CBackend/CBackend.h"
+#include "bistra/Backends/Backend.h"
+#include "bistra/Program/Program.h"
+#include "bistra/Program/Utils.h"
+
+#include <array>
+#include <cstdio>
+#include <fstream>
+#include <iostream>
+#include <memory>
+#include <sstream>
+#include <stdexcept>
+#include <string>
+
+using namespace bistra;
+
