57ab8ef5d2a9ca5447a9429937ddbecffbda6ce7
Nadav Rotem <nrotem@fb.com>
2019-06-01 22:29:09 -0700

    Add the ability to save LL files to disk.

--- include/bistra/Backends/LLVMBackend/LLVMBackend.h
+++ include/bistra/Backends/LLVMBackend/LLVMBackend.h
@@ -5,13 +5,30 @@
 #include "bistra/Program/Program.h"
 
 namespace llvm {
+namespace orc {
+class SimpleJIT;
+}
 class Module;
+class TargetMachine;
 }
 
 namespace bistra {
 
 class LLVMBackend : public Backend {
+  /// An instance of the ORC JIT. Notice that we don't use unique_ptr here
+  /// to work around and create the rtti barrier between our code and LLVM.
+  llvm::orc::SimpleJIT *JIT;
+
+  /// Optimize the module \p M.
+  void optimize(llvm::TargetMachine &TM, llvm::Module *M);
+
+  /// \returns the native target machine.
+  llvm::TargetMachine &getTargetMachine();
+
 public:
+  LLVMBackend();
+  ~LLVMBackend();
+
   virtual std::string emitProgramCode(Program *p) override;
 
   virtual std::string emitBenchmarkCode(Program *p, unsigned iter) override;
